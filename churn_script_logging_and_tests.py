0b2051a8f4d64a384a2df90916f3d44fc037e0a2
fabiobarbazza <fabio.barbazza@dazn.com>
2021-12-04 17:09:22 +0100

    logging_testing file: fix methods

--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -33,21 +33,31 @@ def test_import(pth,import_data):
         logging.error("ERROR: Testing import_data: The file wasn't found: {}".format(err))
  
     try:
+        logging.info("INFO: Testing import_data read file")
+        df= import_data(pth)    
+
         logging.info("SUCCESS: Testing import_data read file")
-        return import_data(pth)    
 
     except BaseException as err:
         logging.error("ERROR: Testing import_data error reading file: {}".format(err))
 
     try:
-        assert df.shape[0] > 0
-        assert df.shape[1] > 0
+        assert df.shape[0] > 0,'zero records'
+        assert df.shape[1] > 0,'zero records'
+        logging.info("SUCCESS: Testing import_data file is not empty")
+
+        return df
 
     except AssertionError as err:
         logging.error("ERROR: Testing import_data: The file doesn't appear to have rows and columns: {}".format(err))
+    
+        #return empty df
+        return pd.DataFrame()   
     except BaseException as err:
         logging.error("ERROR: Testing import_data: dataframe does not exist: {}".format(err))
 
+        #return empty df
+        return pd.DataFrame()
 
 def test_eda(df,perform_eda,pth_folder_plot):
     '''
@@ -57,16 +67,16 @@ def test_eda(df,perform_eda,pth_folder_plot):
         logging.info("INFO: Testing perform_eda")
 
         #check size of dataframe
-        assert all([True if elem > 0 else False for elem in df.shape])
+        assert all([True if elem > 0 else False for elem in df.shape]),'not all columns in dataframe'
 
         logging.info("SUCCESS: Testing perform_eda: dataframe shape > 0")
 
         #check if exists any null values
-        assert df.isnull().sum().sum()==0
+        assert df.isnull().sum().sum()==0,'dataframe contains NULLS'
         logging.info("SUCCESS: Testing perform_eda: no NULL values in input dataframe")
 
         #check if folder to store plot exists
-        assert os.path.exists(pth_folder_plot)==True
+        assert os.path.exists(pth_folder_plot)==True, 'folder does not exist'
         logging.info("SUCCESS: Testing perform_eda: plot folder exists")
 
 
@@ -90,28 +100,28 @@ def test_encoder_helper(encoder_helper,df, category_lst, response):
         logging.info("INFO: Testing test_encoder_helper")
 
         #check size of dataframe
-        assert all([True if elem > 0 else False for elem in df.shape])
+        assert all([True if elem > 0 else False for elem in df.shape]),'empty dataframe'
 
         logging.info("SUCCESS: Testing test_encoder_helper: dataframe shape > 0")
 
         #check if exists any null values
-        assert df.isnull().sum().sum()==0
+        assert df.isnull().sum().sum()==0,'dataframe contains NULLS'
         logging.info("SUCCESS: Testing test_encoder_helper: no NULL values in input dataframe")
 
         #check if category_lst exist
-        assert len(category_lst)>0
+        assert len(category_lst)>0,'category list empty'
         logging.info("SUCCESS: Testing test_encoder_helper: category list does not exist")
 
         #check if response is string
-        assert isinstance(response,str)
+        assert isinstance(response,str),'response variable is not string'
         logging.info("SUCCESS: Testing test_encoder_helper: response is string")
 
         #check if category list are columns in dataframe
-        assert all(elem in list(df.columns)  for elem in category_lst)
+        assert all(elem in list(df.columns)  for elem in category_lst),'category list values are not in columns'
         logging.info("SUCCESS: Testing test_encoder_helper: category list are columns in dataframe")
 
         #check if response refers to a column in dataframe
-        assert response in list(df.columns)
+        assert response in list(df.columns),'response variable is not in dataframe columns'
         logging.info("SUCCESS: Testing test_encoder_helper: response refers to a columns in dataframe")
 
 
@@ -120,13 +130,19 @@ def test_encoder_helper(encoder_helper,df, category_lst, response):
         logging.error("ERROR: Testing test_encoder_helper: error: {}".format(err))    
 
     try:
+        logging.info("INFO: Testing test_encoder_helper method")
+
         # test method
-        return encoder_helper(df,category_lst,response)
+        encoder_return= encoder_helper(df,category_lst,response)
+
         logging.info("SUCCESS: Testing test_encoder_helper: success performing new columns")    
 
-    except:
-        logging.error("ERROR: Testing test_encoder_helper: error performing new columns: {}".format(err))    
+        return encoder_return
+    except BaseException as err :
 
+        logging.error("ERROR: Testing test_encoder_helper: error performing new columns: {}".format(err))    
+        #return empty df
+        return pd.DataFrame()
 
 
     
@@ -159,10 +175,14 @@ def test_perform_feature_engineering(perform_feature_engineering,df,keep_cols,re
     try:
         logging.info("INFO: Testing test_perform_feature_engineering splitting dataframe")
 
-        perform_feature_engineering(df,keep_cols,response)
+        X_train, X_test, y_train, y_test = perform_feature_engineering(df,keep_cols,response)
 
+        logging.info("SUCCESS: Testing test_perform_feature_engineering splitting dataframe")
+        return X_train, X_test, y_train, y_test
     except BaseException as err:
-        logging.error("ERROR: Testing test_perform_feature_engineering: error splitting dataframe: {}".format(err))    
+        logging.error("ERROR: Testing test_perform_feature_engineering: error splitting dataframe: {}".format(err))
+
+        return pd.DataFrame(), pd.DataFrame(),pd.DataFrame(),pd.DataFrame()
 
 
 def test_train_models(train_models,X_train, X_test, y_train, y_test):
@@ -200,22 +220,12 @@ if __name__ == "__main__":
     df=test_import('data/bank_data.csv',cls.import_data)
     df['Churn'] = df['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1)
 
-    #test_eda(df,cls.perform_eda,'images/eda/')
-    #test_encoder_helper(cls.encoder_helper,df,['Gender','Education_Level','Marital_Status','Income_Category','Card_Category'],'Churn')
+    test_eda(df,cls.perform_eda,'images/eda/')
+    
     category_lst=['Gender','Education_Level','Marital_Status','Income_Category','Card_Category']
     response='Churn'
-    for cat in category_lst:
-        cat_lst = []
-
-        #group by current category and get mean of response variable (usually CHURN)
-        cat_groups = df.groupby(cat).mean()[response]
-
-        #create list of values
-        for val in df[cat]:
-                cat_lst.append(cat_groups.loc[val])
-
-        #append current list as new column on input dataframe
-        df[f'{cat}_{response}'] = cat_lst 
+    
+    test_encoder_helper(cls.encoder_helper,df,['Gender','Education_Level','Marital_Status','Income_Category','Card_Category'],'Churn')
 
     
     keep_cols = ['Customer_Age', 'Dependent_count', 'Months_on_book',
